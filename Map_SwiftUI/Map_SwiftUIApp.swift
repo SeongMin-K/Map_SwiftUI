dfa4fcb7c4bf190dd928e142bc854de1f0cf705d
SeongMin-K <eo1tks3@naver.com>
2021-12-30 11:51:38 +0900

    Initial Commit

---
+++ Map_SwiftUI/Map_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_SwiftUIApp.swift
+//  Map_SwiftUI
+//
+//  Created by SeongMinK on 2021/12/30.
+//
+
+import SwiftUI
+
+@main
+struct Map_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
